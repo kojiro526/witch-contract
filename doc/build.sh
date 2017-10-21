271f0a326126336c9f7a231454cf87244b8c672d
kojiro <kojiro@ryusei-sha.com>
2017-10-22 08:37:36 +0900

    ビルドスクリプトを修正

--- doc/build.sh
+++ doc/build.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
+# docxビルドスクリプト
+#
+# Usage:
+#
+# 1.  引数を省略するとデフォルトのファイル名は output.docx
+#     bash ./build.sh
+# 2.  第一引数でファイル名のベースを指定可能。
+#     以下のコマンドで生成されるファイルのは mydocx.docx
+#     bash ./build.sh mydocx
+#
+# 注意
+# 各種の作図ツールは予めインストールされていること。
+# 以下の例で plantuml.jar は ~/bin に格納されているものとする。
+
+NAME_BASE=output
+if [ -n "$1" ]; then
+  NAME_BASE=$1
+  echo -e "NAME_BASE=$NAME_BASE\n"
+fi
+NAME_OUTPUT=$NAME_BASE.docx
+
 # 図を生成
 dot ./**/images/*.dot -Tpng -O
-java -jar ~/Downloads/plantuml.jar ./**/images/*.puml
+java -jar ~/bin/plantuml.jar ./**/images/*.puml
 blockdiag -Tpng --antialias --no-transparency ./**/images/*.blockdiag
 seqdiag -Tpng --antialias --no-transparency ./**/images/*.seqdiag
 actdiag -Tpng --antialias --no-transparency ./**/images/*.actdiag
@@ -11,7 +32,8 @@ nwdiag -Tpng --antialias --no-transparency ./**/images/*.nwdiag
 # ビルド用に画像をコピー
 rm -rf ./images/*
 cp ./**/images/*.png ./images/
+cp ./**/images/*.jpg ./images/
 
 # docxファイルをビルド
-pandoc ./**/*.md --reference-docx=../../c92-book/example/ref/reference-sec.docx --toc --toc-depth=3 -o ./spec.docx
-docxtable-php update -f ./spec.docx -s MyTable
+pandoc ./**/*.md --reference-docx=./template/reference-part.docx --toc --toc-depth=3 -o ./$NAME_OUTPUT
+docxtable-php update -f ./$NAME_OUTPUT -s MyTable
